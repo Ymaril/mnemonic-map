c38302574c436662bccfd51b877cd31cacfbfde9
ymaril <martinsep@yandex.ru>
2025-11-28 00:14:24 +0500

    Fix marker icon

--- src/Map.tsx
+++ src/Map.tsx
@@ -8,6 +8,15 @@ import {
   useMapEvents,
 } from "react-leaflet";
 import type { LatLngLiteral } from "leaflet";
+import L from "leaflet";
+
+// Fix for default markers in production
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+});
 
 type MapProps = {
   value: LatLngLiteral;
