6b9121f837d46bcdd9d31e93248b8f7a49a0c1dd
ymaril <martinsep@yandex.ru>
2025-11-27 21:16:37 +0500

    Improve app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { Map } from "./Map"
+import type { LatLngLiteral } from "leaflet";
+import { MnemonicInput } from "./MnemonicInput";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [point, setPoint] = useState<LatLngLiteral>({ lat: 51.505, lng: -0.09 });
+  const [mnemonic, setMnemonic] = useState("")
+
+  function handleValidChange(full: string, city: string, words: string[]) {
+    console.log("Валидная мнемоника:")
+    console.log("Полное значение:", full)
+    console.log("Город:", city)
+    console.log("Слова:", words)
+  }
 
   return (
     <>
+      <h1>Mnemonic Map</h1>
+      <MnemonicInput
+        value={mnemonic}
+        onChange={setMnemonic}
+        onValidChange={handleValidChange}
+      />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Map value={point} onChange={setPoint} />
+        <pre>{JSON.stringify(point, null, 2)}</pre>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +41,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
